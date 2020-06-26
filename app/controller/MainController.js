8e772393899f02760deaeb4947e664ebb1c9a3a7
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2020-06-26 11:11:22 +0200

    Modification de dernière minutes ? Nan en vrais ajout d'immage et tous voilà

--- app/controller/MainController.js
+++ app/controller/MainController.js
@@ -1,13 +1,18 @@
+var createError = require('http-errors');
+
 class MainController{
 
-    constructor (req,res){
+    constructor (req,res,next){
         this.req = req;
         this.res = res;
+        this.next = next;
     }
 
     steper(){
         if(this.req.params.step>0 && this.req.params.step<=3)
             this.res.render('./nous-rejoindre/step/'+this.req.params.step.toString(), { title: 'Rejoindre' });
+        else
+            this.next(createError(404));
     }
 }
 
